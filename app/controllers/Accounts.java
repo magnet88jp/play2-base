4dc141271da071c4570da3926965931ce131e815
magnet88jp <magnet88jp@gmail.com>
2013-03-24 17:40:31 +0900

    add Procfile

--- app/controllers/Accounts.java
+++ app/controllers/Accounts.java
@@ -10,16 +10,19 @@ import models.Account;
  
 public class Accounts extends Controller {
 
-  public static Result list() {
+//  public static Result list() {
+  public static Result list(Integer page, Integer orderBy, String filter) {
     // 現在のAccountを全検索して表示
     Finder<Long, Account> finder = new Finder<Long, Account>(Long.class, Account.class);
     List<Account> accounts = finder.all();
 
     StringBuilder msg = new StringBuilder();
     msg.append("test");
+    msg.append(filter);
     for (Account account : accounts) {
       msg.append(account.toString()).append("\n");
     }
-    return ok(msg.toString());
+//    return ok(msg.toString());
+      return ok(views.html.Account.list.render(msg.toString(), accounts));
   }
 }
